173805c48451d44a6e439c5b300fdffbfb53f690
anastasia <Anastasia9595@googlemail.com>
2024-02-13 20:49:54 +0100

    add Name and change javascript

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-$(document).ready(function () {
-  $(".container")
-    .mouseenter(function () {
-      $(".card").stop().animate(
-        {
-          top: "-90px",
-        },
-        "slow"
-      );
-    })
-    .mouseleave(function () {
-      $(".card").stop().animate(
-        {
-          top: 0,
-        },
-        "slow"
-      );
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Your code here
+  const card = document.querySelector(".card");
+  const container = document.querySelector(".container");
+  let isCardUp = false;
+
+  container.addEventListener("click", function () {
+    if (!isCardUp) {
+      card.style.top = "-90px";
+      card.style.transition = "0.5s";
+      isCardUp = true;
+    } else {
+      card.style.top = "0";
+      isCardUp = false;
+    }
+  });
 });
