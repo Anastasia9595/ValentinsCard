e9bf9496f87e2dfd8dc626cae25cbcaeb4b75bba
Anastasia9595 <Anastasia9595@googlemail.com>
2026-04-26 13:13:36 +0200

    Refactor card animation and add responsive styles for open state

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Your code here
   const card = document.querySelector(".card");
   const container = document.querySelector(".container");
-  let isCardUp = false;
 
   container.addEventListener("click", function () {
-    if (!isCardUp) {
-      card.style.top = "-180px";
-      card.style.transform = "scale(2)";
-      card.style.transition = "0.5s";
-      card.style.zIndex = "1000";
-      isCardUp = true;
-    } else {
-      card.style.top = "0";
-      card.style.transform = "scale(1)";
-      card.style.zIndex = "0";
-      isCardUp = false;
-    }
+    card.classList.toggle("open");
   });
 });
